a58b16d1c16f6e384dd7ac948924009ac31eb827
Jason Sautieres <jason.sautieres@gmail.com>
2022-03-14 12:11:57 +0100

    Add menu + items

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -83,4 +85,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onResume();
         Log.i(CAT, "onResume");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_compte:
+                alerter("Menu Compte");
+                break;
+            case R.id.menu_pref:
+                alerter("Menu Préférences");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
